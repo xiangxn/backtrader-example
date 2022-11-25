edcd87a20e71d0b216cd25a8eb9dd691910b0189
necklace <xiangxn@163.com>
2022-11-25 21:20:47 +0800

    update bollkdj

--- binance_data.py
+++ binance_data.py
@@ -3,7 +3,7 @@ import backtrader as bt
 
 from data.dataset import CustomDataset
 from strategies.abbration import Abbration
-from strategies.bollkdj import BOLLKDJ
+from strategies.bollkdj import BOLLKDJStrategy
 from strategies.bollema import BollEMA
 from strategies.boll import BollStrategy
 
@@ -21,16 +21,17 @@ class PrintClose(bt.Strategy):
 if __name__ == '__main__':
     cerebro = bt.Cerebro(maxcpus=1)
     # cerebro.addstrategy(PrintClose)
-    cerebro.addstrategy(BollStrategy, period_boll=275, boll_diff=40)
+    # cerebro.addstrategy(BollStrategy, period_boll=275, boll_diff=40)
     # cerebro.addstrategy(BollEMA)
     # cerebro.addstrategy(Abbration, boll_period=200)
-    # cerebro.addstrategy(BOLLKDJ, boll_period=200, kdj_period=72, kdj_ma1=24, kdj_ma2=24)
+    cerebro.addstrategy(BOLLKDJStrategy, period_boll=48)
 
     # cerebro.optstrategy(BollStrategy, period_boll=range(250, 300,5), debug=False)
+    # cerebro.optstrategy(BOLLKDJStrategy, period_boll=48, debug=False)
 
     # 加载数据
     data = CustomDataset(name="ETH",
-                         dataname="data/ETHUSDT-1m-2022.csv",
+                         dataname="data/ETHUSDT-1m-2022-10.csv",
                          dtformat=lambda x: datetime.utcfromtimestamp(int(x) / 1000),
                          timeframe=bt.TimeFrame.Minutes,
                          fromdate=datetime(2022, 5, 1),
--- strategies/bollkdj.py
+++ strategies/bollkdj.py
@@ -1,59 +1,104 @@
 import backtrader as bt
 from indicators.kdj import KDJ
 from datetime import datetime
+from backtrader import Order
 
 
-class BOLLKDJ(bt.Strategy):
-    params = (("boll_period", 21), ("boll_mult", 2), ("kdj_period", 9), ("kdj_ma1", 3), ("kdj_ma2", 3))
+class BOLLKDJStrategy(bt.Strategy):
+    params = (("period_boll", 21), ("boll_mult", 2), ("kdj_period", 9), ("kdj_ma1", 3), ("kdj_ma2", 3), ("price_diff", 20), ("debug", True))
 
     def log(self, txt, dt=None):
+        if not self.p.debug: return
         dt = dt or self.datas[0].datetime.datetime(0)
         print(f'{dt} {txt}')
 
     def __init__(self):
-        self.boll = bt.indicators.BollingerBands(self.datas[0], period=self.p.boll_period, devfactor=self.p.boll_mult)
+        self.trade_count = 0
+        self.boll = bt.indicators.BollingerBands(self.datas[0], period=self.p.period_boll, devfactor=self.p.boll_mult)
         self.kdj = KDJ(self.datas[0], period=self.p.kdj_period, period_dfast=self.p.kdj_ma1, period_dslow=self.p.kdj_ma2)
         # 保存交易状态
         self.marketposition = 0
-        self.last_price = 0
+        self.position_price = 0
+
+        # BOLL信号
+        self.boll_signal = 0
         # KDJ信号
         self.kdj_signal = 0
 
-    def next(self):
+    def notify_order(self, order):
+        if order.exectype == Order.Market and order.status == Order.Completed:
+            self.last_price = order.executed.price
+            self.log(
+                f"Order: {order.ordtypename()}, Status: {order.getstatusname()}, Price: {order.executed.price}, Size: {order.executed.size}, Alive: {order.alive()}"
+            )
+
+    def notify_trade(self, trade):
+        if trade.isclosed:
+            self.trade_count += 1
+
+    def check_boll_signal(self):
         data = self.datas[0]
         up = self.boll.top[0]
         dn = self.boll.bot[0]
-        diff = up - dn
+        # 卖出信号
+        if data.close[0] > up:
+            self.boll_signal = -1
+        # 买入信号
+        elif data.close[0] < dn:
+            self.boll_signal = 1
 
-        # KDJ信号
+    def check_kdj_signal(self):
         condition1 = self.kdj.J[-1] - self.kdj.D[-1]
         condition2 = self.kdj.J[0] - self.kdj.D[0]
-        if condition1 < 0 and condition2 > 0:
+        if condition1 < 0 and condition2 > 0 and self.kdj.D[0] <= 25:
             self.kdj_signal = 1  # 上穿(买入信号)
-        elif condition1 > 0 and condition2 < 0:
+        elif condition1 > 0 and condition2 < 0 and self.kdj.D[0] >= 65:
             self.kdj_signal = -1  # 下穿(卖出信号)
+
+    def dn_across(self, data):
+        return data.close[-1] > self.boll.mid[-1] and data.close[0] < self.boll.mid[0]
+
+    def up_across(self, data):
+        return data.close[-1] < self.boll.mid[-1] and data.close[0] > self.boll.mid[0]
+
+    def next(self):
+        # BOLL信号检查
+        self.check_boll_signal()
+        # KDJ信号
+        self.check_kdj_signal()
 
         # 未持仓
         if self.marketposition == 0:
             # 买入
-            if data.close[0] > up and self.kdj_signal == 1 and diff > 50:
-                self.buy(data)
-                self.last_price = data.close[0]
+            if self.boll_signal > 0 and self.kdj_signal > 0:
+                self.buy(self.datas[0])
                 self.marketposition = 1
+                self.boll_signal = 0
+                self.kdj_signal = 0
             # 卖出
-            if data.close[0] < dn and self.kdj_signal == -1 and diff > 50:
-                self.sell(data)
-                self.last_price = data.close[0]
+            elif self.boll_signal < 0 and self.kdj_signal < 0:
+                self.sell(self.datas[0])
                 self.marketposition = -1
+                self.boll_signal = 0
+                self.kdj_signal = 0
         # 已持空仓
         elif self.marketposition == -1:
-            if (self.kdj_signal == 1 and data.close[0] > dn) or (data.close[0] - self.last_price > 20):
+            # if self.boll_signal > 0 and self.kdj_signal > 0:
+            if self.dn_across(self.datas[0]):
                 self.close()
                 self.marketposition = 0
                 self.last_price = 0
+                self.boll_signal = 0
+                self.kdj_signal = 0
         # 已持多仓
         else:
-            if (self.kdj_signal == -1 and data.close[0] < up) or (self.last_price - data.close[0] > 20):
+            # if self.boll_signal < 0 and self.kdj_signal < 0:
+            if self.up_across(self.datas[0]):
                 self.close()
                 self.marketposition = 0
                 self.last_price = 0
+                self.boll_signal = 0
+                self.kdj_signal = 0
+
+    def stop(self):
+        print('(MA period_boll %2d) Ending Value: %.2f, Trade Count: %d' % (self.p.period_boll, self.broker.getcash(), self.trade_count))
