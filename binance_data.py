bb1584d09c2188c0c138cc135dc96a8cc163084f
necklace <xiangxn@163.com>
2023-02-11 12:15:49 +0800

    add new strategy

--- binance_data.py
+++ binance_data.py
@@ -8,6 +8,7 @@ from strategies.bollkdj import BOLLKDJStrategy
 from strategies.bollema import BollEMA
 from strategies.boll import BollStrategy
 from strategies.macdkdj import MACDKDJStrategy
+from strategies.boll_reverser import BollReverser
 
 
 class PrintClose(bt.Strategy):
@@ -24,22 +25,25 @@ if __name__ == '__main__':
     logging.config.fileConfig("logging.ini")
     cerebro = bt.Cerebro(maxcpus=1)
     # cerebro.addstrategy(PrintClose)
-    cerebro.addstrategy(BollStrategy, period_boll=275)
+    # cerebro.addstrategy(BollStrategy, period_boll=275)
     # cerebro.addstrategy(BollEMA)
     # cerebro.addstrategy(Abbration, boll_period=200)
     # cerebro.addstrategy(BOLLKDJStrategy, price_diff=30)
     # cerebro.addstrategy(MACDKDJStrategy)
+
+    cerebro.addstrategy(BollReverser)
+    # cerebro.optstrategy(BollReverser, period_boll=range(40, 60, 2), debug=False)
 
     # cerebro.optstrategy(BollStrategy, period_boll=range(20, 300,20), debug=False)
     # cerebro.optstrategy(BOLLKDJStrategy, price_diff=range(5, 50,5), debug=False)
 
     # 加载数据
     data = CustomDataset(name="ETH",
-                         dataname="data/ETHUSDT-1m-2022-05.csv",
+                         dataname="data/BTCUSDT-1m-2022-05.csv",
                          dtformat=lambda x: datetime.utcfromtimestamp(int(x) / 1000),
                          timeframe=bt.TimeFrame.Minutes,
                          fromdate=datetime(2022, 5, 1),
-                         todate=datetime(2022, 11, 1),
+                         todate=datetime(2022, 12, 31),
                          nullvalue=0.0)
     cerebro.resampledata(data, timeframe=bt.TimeFrame.Minutes, compression=5)
 
@@ -48,10 +52,11 @@ if __name__ == '__main__':
     # 配置滑点费用,2跳
     # cerebro.broker.set_slippage_fixed(slippage*1)
 
-    cerebro.broker.setcommission(commission=0.0004, margin=0.1, mult=1.0)
+    # cerebro.broker.setcommission(commission=0.0004, margin=0.1, mult=1.0)
     # cerebro.broker.setcommission(commission=0.00075)
 
-    cerebro.addsizer(bt.sizers.FixedSize, stake=1)
+    # cerebro.addsizer(bt.sizers.FixedSize, stake=1)
+    cerebro.addsizer(bt.sizers.PercentSizer, percents=50)
 
     # cerebro.addwriter(bt.WriterFile, out='log.csv', csv=True)
 
---
+++ strategies/boll_reverser.py
@@ -0,0 +1,99 @@
+import json
+import logging
+import signal
+import backtrader as bt
+from backtrader import Order
+from datetime import datetime
+
+
+class BollReverser(bt.Strategy):
+    params = (("period_boll", 52), ("production", False), ("debug", True))
+
+    logger = None
+
+    def debug(self, txt, dt=None):
+        dt = dt or self.datas[0].datetime.datetime(0)
+        self.logger.debug(f'[{dt}]: {txt}')
+
+    def warning(self, txt):
+        self.logger.warning(txt)
+
+    def __init__(self) -> None:
+        signal.signal(signal.SIGINT, self.sigstop)
+        self.boll = bt.indicators.bollinger.BollingerBands(self.datas[0], period=self.p.period_boll)
+        self.logger = self.logger if self.logger else logging.getLogger(__name__)
+        if self.p.debug:
+            self.logger.setLevel(logging.DEBUG)
+        else:
+            self.logger.setLevel(logging.WARNING)
+        self.trade_count = 0
+
+    def sigstop(self, a, b):
+        self.warning('Stopping Backtrader...')
+        self.env.runstop()
+
+    def stop(self):
+        self.warning('(MA Period_boll %2d) Ending Value: %.2f, Trade Count: %d' % (self.p.period_boll, self.broker.getcash(), self.trade_count))
+
+    def notify_trade(self, trade):
+        if trade.isclosed:
+            self.trade_count += 1
+            symbol = trade.getdataname()
+            pnl = trade.pnl
+            pnlcomm = trade.pnlcomm
+            value = self.broker.getvalue()
+            cash = self.broker.getcash()
+            self.debug(f'closed symbol is : {symbol}, total_profit : {pnl}, net_profit : {pnlcomm}, value: {value}, cash: {cash} count: {self.trade_count}')
+        if trade.isopen:
+            self.debug(f'open symbol is : {trade.getdataname()} , price : {trade.price}, size: {trade.size} ')
+
+    def next(self):
+        position = self.getposition()
+
+        if position.size == 0:
+            # if self.up_across_top():
+            if self.close_gt_up():
+                self.sell()
+            # elif self.down_across_bot():
+            elif self.close_lt_dn():
+                self.buy()
+        elif position.size > 0:
+            # if self.down_across_mid():
+            if self.close_across_top():
+                self.close()
+        elif position.size < 0:
+            # if self.up_across_mid():
+            if self.close_across_bot():
+                self.close()
+
+    def down_across_mid(self):
+        data = self.datas[0]
+        return data.close[-1] > self.boll.mid[-1] and data.close[0] < self.boll.mid[0]
+
+    def up_across_mid(self):
+        data = self.datas[0]
+        return data.close[-1] < self.boll.mid[-1] and data.close[0] > self.boll.mid[0]
+
+    def down_across_bot(self):
+        data = self.datas[0]
+        return data.close[-2] > self.boll.bot[-2] and data.close[-1] > self.boll.bot[-1] and data.close[0] < self.boll.bot[0]
+
+    def close_across_bot(self):
+        data = self.datas[0]
+        return data.close[-1] > self.boll.bot[-1] and data.close[0] < self.boll.bot[0]
+
+    def up_across_top(self):
+        data = self.datas[0]
+        return data.close[-2] < self.boll.top[-2] and data.close[-1] < self.boll.top[-1] and data.close[0] > self.boll.top[0]
+
+    def close_across_top(self):
+        data = self.datas[0]
+        return data.close[-1] < self.boll.top[-1] and data.close[0] > self.boll.top[0]
+
+    def close_gt_up(self):
+        data = self.datas[0]
+        return data.close[0] > self.boll.top[0] and data.close[-1] > self.boll.top[-1]
+
+    def close_lt_dn(self):
+        data = self.datas[0]
+        return data.close[0] < self.boll.bot[0] and data.close[-1] < self.boll.bot[-1]
