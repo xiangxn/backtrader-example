38915f705222fcef82526fb2af6fd9b3b5393a13
necklace <xiangxn@163.com>
2023-05-15 17:39:09 +0800

    add period param

--- binance_live.py
+++ binance_live.py
@@ -9,51 +9,12 @@ from utils.helper import init_env, get_env
 import logging.config
 import argparse
 
-
-class TestStrategy(bt.Strategy):
-
-    def __init__(self):
-
-        self.sma = bt.indicators.SMA(self.data, period=21)
-
-    def next(self):
-
-        # Get cash and balance
-        # New broker method that will let you get the cash and balance for
-        # any wallet. It also means we can disable the getcash() and getvalue()
-        # rest calls before and after next which slows things down.
-
-        # NOTE: If you try to get the wallet balance from a wallet you have
-        # never funded, a KeyError will be raised! Change LTC below as approriate
-        if self.live_data:
-            cash, value = self.broker.get_wallet_balance('USDT')
-        else:
-            # Avoid checking the balance during a backfill. Otherwise, it will
-            # Slow things down.
-            cash = 'NA'
-            # return  # 仍然处于历史数据回填阶段，不执行逻辑，返回
-
-        for data in self.datas:
-
-            print('{} - {} | Cash {} | O: {} H: {} L: {} C: {} V:{} SMA:{}'.format(data.datetime.datetime(), data._name, cash, data.open[0], data.high[0],
-                                                                                   data.low[0], data.close[0], data.volume[0], self.sma[0]))
-
-    def notify_data(self, data, status, *args, **kwargs):
-        dn = data._name
-        dt = datetime.utcnow()
-        msg = 'Data Status: {}'.format(data._getstatusname(status))
-        print(dt, dn, msg)
-        if data._getstatusname(status) == 'LIVE':
-            self.live_data = True
-        else:
-            self.live_data = False
-
-
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser(prog=sys.argv[0], formatter_class=argparse.RawDescriptionHelpFormatter)
     arg_parser.add_argument('-R', '--reversal', action="store_true", help='Whether to flip the transaction')
     arg_parser.add_argument('-C', '--clear', action="store_true", help='Whether to clear the status')
     arg_parser.add_argument('-S', '--stake', type=int, help='set stake', default=1)
+    arg_parser.add_argument('-P', '--period', type=int, help='set period', default=140)
     args = arg_parser.parse_args(args=sys.argv[1:])
 
     if args.clear:
@@ -64,9 +25,8 @@ if __name__ == '__main__':
     cerebro = bt.Cerebro()
 
     # Add the strategy
-    # cerebro.addstrategy(TestStrategy)
     # cerebro.addstrategy(BollEMA, period_boll=200, period_ema=99, production=True)
-    cerebro.addstrategy(BollStrategy, production=True, reversal=args.reversal)
+    cerebro.addstrategy(BollStrategy, production=True, period_boll=args.period, reversal=args.reversal)
 
     # Create our store
     config = { 'apiKey': get_env('B_APIKEY'), 'secret': get_env('B_SECRET'), 'enableRateLimit': True }
